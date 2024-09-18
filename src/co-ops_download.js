6e6f2ebd5019cb6bc11e89f8859685931691786a
Quincy Morgan <2046746+quincylvania@users.noreply.github.com>
2024-09-18 09:29:05 -0400

    Add downloader for NOAA CO-OPS data

---
+++ src/co-ops_download.js
@@ -0,0 +1,71 @@
+import { writeFileSync } from 'fs';
+import { get, clearDirectory } from './utils.js';
+
+// Fetch official site data from NOAA CO-OPS Metadata API:
+// https://api.tidesandcurrents.noaa.gov/mdapi/prod/
+
+clearDirectory('./co-ops/source/');
+
+console.log('Fetching latest NOAA CO-OPS stations…');
+
+// There isn't one endpoint for all stations, so we need to fetch different endpoints that add up to all stations.
+// It's not clear exactly which types intersect so just get everything. 
+let types = [
+  "waterlevels",
+  "waterlevelsandmet",
+  "airgap",
+  "datums",
+  "supersededdatums",
+  "benchmarks",
+  "supersededbenchmarks",
+  "historicwl",
+  "met",
+  "harcon",
+  "tidepredictions",
+  "currentpredictions",
+  "currents",
+  "historiccurrents",
+  "surveycurrents",
+  "cond",
+  "watertemp",
+  "physocean",
+  "tcoon",
+  "visibility",
+  "1minute",
+  "historicmet",
+  "historicphysocean",
+  "highwater",
+  "lowwater",
+  "hightideflooding",
+  //"ofs",                  // Operational Forecast System stations, out of scope for now
+  //"partnerstations"       // only a few of these, out of scope for now
+];
+
+let stationsById = {};
+
+for (let i in types) {
+  let type = types[i];
+  // get all fields for all features with coordinates in WGS 84
+  const url = `https://api.tidesandcurrents.noaa.gov/mdapi/prod/webapi/stations.json?type=${type}&expand=details,sensors`;
+  console.log(`Fetching: ${url}`);
+  let response = await get(url);
+  let json = JSON.parse(response);
+  if (json && json.stations) {
+    for (let j in json.stations) {
+      let station = json.stations[j];
+      let id = station.id;
+      if (!stationsById[id]) stationsById[id] = station;
+    }
+  } else {
+    console.log(`Invalid response`);
+  }
+}
+
+const json = {
+  count: Object.keys(stationsById).length,
+  units: null,
+  stations: Object.values(stationsById)
+};
+
+console.log(`Writing data to './co-ops/source/all.json'`);
+writeFileSync('./co-ops/source/all.json', JSON.stringify(json, null, 2));
